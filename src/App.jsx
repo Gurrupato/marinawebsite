f2208b69fb32aa39a98152032bf3974d7f0316ea
Manuel <105855842+Gurrupato@users.noreply.github.com>
2023-10-24 11:29:18 +0200

    Console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
-
+  console.log("Hola");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button onClick={() => setCount(count => count + 1)}>
+          <button onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
         <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
+          Edit <code>App.jsx</code> and save to test hot module replacement
+          (HMR).
         </p>
         <p>
           <a
@@ -27,7 +28,7 @@ const App = () => {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
